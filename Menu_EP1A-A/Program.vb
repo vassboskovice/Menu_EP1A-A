c1584b2f306bf719693f4eefacfb4ee170819d74
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-26 13:41:08 +0100

    Posunuté menu

--- Menu_EP1A-A/Program.vb
+++ Menu_EP1A-A/Program.vb
@@ -1,7 +1,24 @@
-Imports System
+﻿Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Menu()
+    End Sub
+
+    Sub Menu()
+        Console.SetCursorPosition(20, 5)
+        Console.WriteLine("╔══════════════════╗")
+        Console.SetCursorPosition(20, 6)
+        Console.WriteLine("║      NADPIS      ║")
+        Console.SetCursorPosition(20, 7)
+        Console.WriteLine("╠══════════════════╣")
+        Console.SetCursorPosition(20, 8)
+        Console.WriteLine("║ Položka 1        ║")
+        Console.SetCursorPosition(20, 9)
+        Console.WriteLine("║ Položka 2        ║")
+        Console.SetCursorPosition(20, 10)
+        Console.WriteLine("║ Položka 3        ║")
+        Console.SetCursorPosition(20, 11)
+        Console.WriteLine("╚══════════════════╝")
     End Sub
 End Module
